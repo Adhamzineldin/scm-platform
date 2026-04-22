979e7ebcfb0a9919a34edb2ac242919c6bffa15f
Mohamed Ali <mooomali15@gmail.com>
2026-04-22 13:38:10 +0200

    Add-the-Cart-Service-Interface

--- backend/Cart-service/src/main/java/com/scm/cart/service/CartService.java
+++ backend/Cart-service/src/main/java/com/scm/cart/service/CartService.java
@@ -1,6 +1,17 @@
 package com.scm.cart.service;
 
+import com.scm.cart.entity.CartItem;
+import java.util.List;
+
 public interface CartService {
-    void addToCart(Long userId, Long productId, int quantity);
-    void removeFromCart(Long userId, Long productId, int quantity);
+
+    void addItemToCart(Long userId, Long productId, int quantity);
+
+    List<CartItem> getCartByUserId(Long userId);
+
+    void updateItemQuantity(Long userId, Long productId, int quantity);
+
+    void removeItemFromCart(Long userId, Long productId);
+
+    void clearCartByUserId(Long userId);
 }
